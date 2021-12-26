3ceed66a734636373e9bed0e47c9b1901eeeb6d6
telman03 <telmangadimov1@gmail.com>
2021-12-26 18:53:04 +0400

    mail sender

--- send_mail/settings.py
+++ send_mail/settings.py
@@ -34,11 +34,11 @@ from decouple import config
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('smtp.gmail.com')
+EMAIL_HOST = config('smtp.gmail.com')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('telmangadimov1@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('nbpmeoncbngxseto')#past the key or password app here
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')#past the key or password app here
 
 
 RECIPIENT_ADDRESS = env('telmangadimov2@gmail.com')
